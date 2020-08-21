c96c021aad34ae901ca8fec7e48cdeca102f1cea
Hamidkhan-web <hamidulislamkhan2@gmail.com>
2020-08-21 17:04:24 +0600

    32.6 CREATE COMPONENT, RETURN MULTIPLE HTML FROM A COMPONENT

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var person = {
-    name: "Dr. Mahfuz",
-    job: "singer"
-}
-var person2 = {
-  name: "Eva Rahman",
-  job: "Kokil Konthi"
-}
-var style={
-  color:'red',
-  backgroundColor:'yellow'
-}
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Dones  <code>src/App.js</code> and save to reload.
-        </p>
-  <h1 className="" style={style}>My heading: {person.name +" "+ person.job}</h1>
-  <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Singer: {person2.name +" "+ person2.job}</h3>
-        <p>My fast react paragraph</p>
+        <p> i am a react person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+       
        
       </header>
     </div>
   );
+}
+function Person(){
+  const personStyle={
+    border:'2px solid red',
+    margin:'10px'
+
+  }
+  return (
+  <div style={{border: '2px solid yellow', margin: '10px'}} >
+     <h1>Name: Sakib Al Hasan </h1>
+     <h3>Hero of the year</h3>
+  </div>
+  )
 }
 
 export default App;
