cf607b2a9c40dc918691274e008cc21a76e05a45
Hamidkhan-web <hamidulislamkhan2@gmail.com>
2020-08-21 15:49:03 +0600

    32.4 JSX, DYNAMIC CONTENT, DYNAMIC STYLE IN REACT

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dr. Mahfuz",
+    job: "singer"
+}
+var person2 = {
+  name: "Eva Rahman",
+  job: "Kokil Konthi"
+}
+var style={
+  color:'red',
+  backgroundColor:'yellow'
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +22,9 @@ function App() {
         <p>
           Edit Dones  <code>src/App.js</code> and save to reload.
         </p>
+  <h1 className="" style={style}>My heading: {person.name +" "+ person.job}</h1>
+  <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Singer: {person2.name +" "+ person2.job}</h3>
+        <p>My fast react paragraph</p>
        
       </header>
     </div>
