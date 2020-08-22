5db6f3e7db557189fd829f5c82285258a04fff1a
Hamidkhan-web <hamidulislamkhan2@gmail.com>
2020-08-22 12:10:01 +0600

    32.9 CREATE MULTIPLE COMPONENTS FROM AN ARRAY OF OBJECTS

--- src/App.js
+++ src/App.js
@@ -3,22 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const nayoks = ['Anwar', 'jafor', 'Alomgir', 'Salman']
+  const nayoks = [ 'Razzak', 'jasim', 'Anwar', 'jafor', 'Alomgir', 'Salman']
+  const products = [
+    {name:'Photoshop', price:'$90.99'},
+    {name:'Illustrator', price:'$60.99'},
+    {name:'PDF Reader', price:'$6.99'}
+  ]
+  // const nayokName = nayoks.map(nayok => nayok);
+  // console.log(nayokName);
+  // const productNames = products.map(product => product)
+  // console.log(productNames);
  
 
   return (
     <div className="App">
       <header className="App-header">
         <p> i am a react person</p>
+        {
+          nayoks.map(nayok => <li>{nayok}</li>)
+        }
+        {
+          products.map(product =><li>{product.name}</li>)
+        }
+        {
+          products.map(product =><Product product={product}></Product>)
+        }
+        {/* <Product product={products[0]} ></Product>
+        <Product product={products[1]} ></Product>
+        <Product product={products[2]} ></Product>
        <Person name="Hamidul" job="Web developer"></Person>
        <Person name="Mahim" job="Electric"></Person>
-       <Person></Person>
+       <Person></Person> */}
        <Person></Person>
        
        
       </header>
     </div>
   );
+}
+function Product(props) {
+  const productStyle={
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  const {name, price} = props.product;
+  return(
+    <div style={productStyle}>
+      <h3>{name}</h3>
+  <h5>{price}</h5>
+      <button>Bye now</button>
+
+    </div>
+  )
 }
 function Person(props){
   return (
